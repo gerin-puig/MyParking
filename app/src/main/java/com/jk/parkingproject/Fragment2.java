6a2b413560efc596d0cd2468cd58f6aa1e58f281
rajdeepdodiya <rajdodiya216@gmail.com>
2021-06-03 15:10:49 -0400

    Implemented Tab Bar for Viewing Parking and User Profile.
    Add new parking activity with validations.
    
    Firebase coming up in next commit.

---
+++ app/src/main/java/com/jk/parkingproject/Fragment2.java
@@ -0,0 +1,22 @@
+package com.jk.parkingproject;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jk.parkingproject.R;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Fragment2 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment2_layout, container, false);
+    }
+}
