1b4362322af7785f45eeec8a358fedb779862748
gerin-puig <69816174+gerin-puig@users.noreply.github.com>
2021-06-03 01:15:39 -0400

    -added firebase, firestore, auth in project

--- app/src/main/java/com/jk/parkingproject/MainActivity.java
+++ app/src/main/java/com/jk/parkingproject/MainActivity.java
@@ -1,15 +1,73 @@
 package com.jk.parkingproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.jk.parkingproject.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private ActivityMainBinding binding;
+
+
+//will move
+    private FirebaseAuth fba = FirebaseAuth.getInstance();
+
+    private String TAG = "MyDebug";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.btnLogin.setOnClickListener(this);
+        binding.editEmail.setOnClickListener(this);
+        binding.editPassword.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v != null){
+            switch (v.getId()){
+                case R.id.btnLogin:
+                    login();
+                    break;
+            }
+        }
+    }
+
+    private void login(){
+        String email = binding.editEmail.getText().toString();
+        String password = binding.editPassword.getText().toString();
+
+        if (email.isEmpty() || password.isEmpty()){
+            return;
+        }
+
+        fba.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Log.d(TAG, "onComplete: User Found");
+                    //FirebaseUser user = fba.getCurrentUser();
+
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
